198394061dd53576d4d03bbf6aaee5814b591022
dada4747 <adsurerahul96@gmail.com>
2021-05-18 10:15:58 +0530

    ADDED: fuctionallity to logging

---
+++ src/main/java/com/bridgelabz/logger_practice/App.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.logger_practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
